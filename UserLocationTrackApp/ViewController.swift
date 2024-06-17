c77a9230a14c3d1b5b341e88dca0e08688113c7e
Hideo Uhara <hideo.uhara@gmail.com>
2024-06-17 17:44:26 +0900

    Fixed using MainActor.assumeIsolated for Swift 6

--- UserLocationTrackApp/ViewController.swift
+++ UserLocationTrackApp/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
 		self.mapView.showsUserLocation = true
 		self.mapView.showsScale = true
 		
-		self.locationManager = CLLocationManager()
+		self.locationManager = CLLocationManager() // メインスレッドで作成しているのでdelegateメソッドはメインスレッドで呼ばれる
 		self.locationManager.delegate = self // locationManagerDidChangeAuthorizationが呼び出される
 		
 		self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
@@ -51,68 +51,74 @@ class ViewController: UIViewController {
 extension ViewController: CLLocationManagerDelegate {
 	
 	// 設定アプリにあるこのアプリの設定の変更でもコールされる
-	func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+	nonisolated func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
 		
-		switch manager.authorizationStatus {
-		case .notDetermined:
-			self.locationManager.requestWhenInUseAuthorization() // 位置情報取得の許可ダイアログ表示
-		case .denied:
-			let defaultAction: UIAlertAction = UIAlertAction(title: NSLocalizedString("OK", comment: "OK"), style: .default) { (action) in
-				
-				// ローカライズファイルがあれば設定アプリにこのアプリの設定が表示される。(たぶん、言語の切り替え設定のため)
-				UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
-			}
-			self.showDialog(message: NSLocalizedString("Please enable location service settings.", comment: ""), actions: [defaultAction])
-		case .restricted:
-			self.showDialog(message: NSLocalizedString("Location services are unavailable.", comment: ""))
-		case .authorizedAlways:
-			fallthrough
-		case .authorizedWhenInUse:
-			switch manager.accuracyAuthorization {
-			case .reducedAccuracy: // 位置情報取得許可ダイアログで取得可で正確な位置情報がオフの場合
-				// 正確な位置情報利用の許可確認
-				manager.requestTemporaryFullAccuracyAuthorization(withPurposeKey: "NSLocationTemporaryUsageDescription") { error in
+		MainActor.assumeIsolated {
+			
+			switch self.locationManager.authorizationStatus {
+			case .notDetermined:
+				self.locationManager.requestWhenInUseAuthorization() // 位置情報取得の許可ダイアログ表示
+			case .denied:
+				let defaultAction: UIAlertAction = UIAlertAction(title: NSLocalizedString("OK", comment: "OK"), style: .default) { (action) in
 					
-					if error != nil {
-						NSLog(error!.localizedDescription)
-						return
-					}
-					
-					if manager.accuracyAuthorization == .reducedAccuracy {
-						let defaultAction: UIAlertAction = UIAlertAction(title: NSLocalizedString("OK", comment: "OK"), style: .default) { (action) in
-							
-							UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
-						}
-						self.showDialog(message: NSLocalizedString("This app need precise location information.", comment: ""), actions: [defaultAction])
-					}
+					// ローカライズファイルがあれば設定アプリにこのアプリの設定が表示される。(たぶん、言語の切り替え設定のため)
+					UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
 				}
-			case .fullAccuracy:
-				self.locationManager.startUpdatingLocation()
-				self.mapView.setUserTrackingMode(.followWithHeading, animated: true)
+				self.showDialog(message: NSLocalizedString("Please enable location service settings.", comment: ""), actions: [defaultAction])
+			case .restricted:
+				self.showDialog(message: NSLocalizedString("Location services are unavailable.", comment: ""))
+			case .authorizedAlways:
+				fallthrough
+			case .authorizedWhenInUse:
+				switch self.locationManager.accuracyAuthorization {
+				case .reducedAccuracy: // 位置情報取得許可ダイアログで取得可で正確な位置情報がオフの場合
+					// 正確な位置情報利用の許可確認
+					self.locationManager.requestTemporaryFullAccuracyAuthorization(withPurposeKey: "NSLocationTemporaryUsageDescription") { error in
+						
+						if error != nil {
+							NSLog(error!.localizedDescription)
+							return
+						}
+						
+						if self.locationManager.accuracyAuthorization == .reducedAccuracy {
+							let defaultAction: UIAlertAction = UIAlertAction(title: NSLocalizedString("OK", comment: "OK"), style: .default) { (action) in
+								
+								UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
+							}
+							self.showDialog(message: NSLocalizedString("This app need precise location information.", comment: ""), actions: [defaultAction])
+						}
+					}
+				case .fullAccuracy:
+					self.locationManager.startUpdatingLocation()
+					self.mapView.setUserTrackingMode(.followWithHeading, animated: true)
+				default:
+					break
+				}
 			default:
 				break
 			}
-		default:
-			break
 		}
 	}
 	
-	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+	nonisolated func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
 		
-		if let location: CLLocation = locations.last { // 最後の位置データが最新
+		MainActor.assumeIsolated {
 			
-			if location.horizontalAccuracy < 0.0 { // データ不正の場合
-				return
+			if let location: CLLocation = locations.last { // 最後の位置データが最新
+				
+				if location.horizontalAccuracy < 0.0 { // データ不正の場合
+					return
+				}
+				
+				self.locList.append(CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude))
+				
+				// 軌跡の表示
+				if self.polyline != nil {
+					self.mapView.removeOverlay(self.polyline)
+				}
+				self.polyline = MKPolyline(coordinates: self.locList, count: self.locList.count)
+				self.mapView.insertOverlay(self.polyline, at: 0)
 			}
-			
-			self.locList.append(CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude))
-			
-			// 軌跡の表示
-			if self.polyline != nil {
-				self.mapView.removeOverlay(self.polyline)
-			}
-			self.polyline = MKPolyline(coordinates: self.locList, count: self.locList.count)
-			self.mapView.insertOverlay(self.polyline, at: 0)
 		}
 	}
 }
